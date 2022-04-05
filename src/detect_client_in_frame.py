1672b8815717b735f4e42199f60836c7657b3825
redom69 <48015133+redom69@users.noreply.github.com>
2022-04-05 11:38:55 +0200

    [GH-25] Refractor code

--- src/detect_client_in_frame.py
+++ src/detect_client_in_frame.py
@@ -3,14 +3,13 @@ import cv2
 
 class FrameDetection:
     """
-    Class implements Yolo5 model to make inferences on a youtube video using OpenCV.
+    Class implements Yolo5 model to make inferences on a video using OpenCV.
     """
 
     def __init__(self, frame):
         """
-        Initializes the class with youtube url and output file.
-        :param url: Has to be as youtube URL,on which prediction is made.
-        :param out_file: A valid output file name.
+        Initializes the class with the video frame.
+        :param frame: Frame detected by the video.
         """
         self.frame = frame
         self.bounding_box = []
@@ -34,8 +33,9 @@ class FrameDetection:
         Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
         :param results: contains labels and coordinates predicted by model on the given frame.
         :param frame: Frame which has been scored.
-        :return: Frame with bounding boxes and labels ploted on it.
+        :return: Frame with bounding boxes and labels ploted on it, list of x and list of y coordinates.
         """
+        bounding_box = []
         self.bounding_box = []
         labels, cord = results
         n = len(labels)
@@ -52,21 +52,8 @@ class FrameDetection:
                 self.bounding_box.append([x1, x1, x2, y2])
                 lista_y2.append(y2)
                 lista_x2.append(x2)
-        return frame, lista_y2, lista_x2
-
-    @staticmethod
-    def test_bounding_box(results, frame):
-        bounding_box = []
-        labels, cord = results
-        n = len(labels)
-        x_shape, y_shape = frame.shape[1], frame.shape[0]
-        for i in range(n):
-            row = cord[i]
-            if row[4] >= 0.2:
-                x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(
-                    row[3] * y_shape)
                 bounding_box.append([x1, x1, x2, y2])
-        return bounding_box
+        return frame, lista_y2, lista_x2, bounding_box
 
     @staticmethod
     def entran(lista_y2, lista_y2_ant):
@@ -76,7 +63,6 @@ class FrameDetection:
         :param lista_y2_ant: contains y coordinates of lower right corner from the bounding box (previous frame)
         :return: number of people going into the shop in a frame
         """
-
         cont_ent = 0
         for elem in lista_y2:
             if (elem == 140) and (141 in lista_y2_ant):
@@ -91,7 +77,6 @@ class FrameDetection:
         :param lista_x2: contains x coordinates of lower right corner from the bounding box (current frame)
         :return: number of people going through line 1 (c1) and line 2 (c2) in a frame
         """
-
         c1 = 0
         c2 = 0
         for elem in lista_x2:
@@ -110,13 +95,12 @@ class FrameDetection:
         of lower right corner from the bounding box (previous 10 frames) :param posicion: x coordinate of lower right
         corner from the bounding box if counter is increased :return: number of people stopping in a frame
         """
-
         cont_par = 0
         cont = 0
         for x in pos_x:
             if (x != posicion) and (60 < x < 210):
                 for lista in list(anteriores):
-                    if x in lista:  # contains
+                    if x in lista:
                         cont += 1
                 if cont == 10:
                     cont_par += 1
--- src/read_video.py
+++ src/read_video.py
@@ -9,14 +9,13 @@ class VideoDetection:
 
     def __init__(self, video, out_file):
         """
-        Initializes the class with youtube url and output file.
-        :param url: Has to be as youtube URL,on which prediction is made.
+        Initializes the class with the input video and output file.
+        :param video: Has to be a video.
         :param out_file: A valid output file name.
         """
         self._video = video
         self.model = self.load_model()
         self.out_file = out_file
-        self.test_frame = []
         self.bounding_box_test = []
         self.videoIsClosed = False
         self.entran = 0
@@ -26,7 +25,7 @@ class VideoDetection:
     def get_video(self):
         """
         Creates a new video streaming object to extract video frame by frame to make prediction on.
-        :return: opencv2 video capture object, with lowest quality frame available for video.
+        :return: opencv2 video capture object.
         """
         return cv2.VideoCapture(self._video)
 
@@ -40,24 +39,17 @@ class VideoDetection:
 
         return model
 
-    def get_test_frame(self):
-        """
-        Function for unitary tests
-        :return:  Test frame for unitary test
-        """
-        return self.test_frame
-
     def count_frames_manually(self):
+        """
+        Function for progressing bar
+        :return: void
+        """
         frame_count = 0
         video = self.get_video()
-        # Iteraremos sobre cada "frame" en el video, incrementando el conteo.
         while True:
             frame_was_read, _ = video.read()
-
-            # Dejamos de iterar cuando ya no haya más cuadros en el video.
             if not frame_was_read:
                 return frame_count
-
             frame_count += 1
 
     def __call__(self):
@@ -66,7 +58,6 @@ class VideoDetection:
         and write the output into a new file.
         :return: void
         """
-        bbx = None
         c_entran = []
         c_l1 = []
         c_l2 = []
@@ -87,7 +78,7 @@ class VideoDetection:
                 break
             fd = detect_client_in_frame.FrameDetection(frame)
             results = fd.score_frame(frame, self.model)
-            frameout, pos_y, pos_x = fd.plot_boxes(results, frame)
+            frameout, pos_y, pos_x, bbx = fd.plot_boxes(results, frame)
             x_anteriores.appendleft(pos_x)
             ent = fd.entran(pos_y, pos_y_ant)
             l1, l2 = fd.pasan(pos_x)
@@ -97,11 +88,9 @@ class VideoDetection:
             c_l1.append(l1)
             c_l2.append(l2)
             c_paran.append(sp)
-            if len(fd.bounding_box) > 0:
-                self.test_frame = frame
-                bbx = fd.test_bounding_box(results, frame)
             out.write(frameout)
-            self.bounding_box_test = bbx
+            if len(bbx) > 0:
+                self.bounding_box_test = bbx
             self.videoIsClosed = True
             self.entran = sum(c_entran)
             self.no_pasan = min(sum(c_l1), sum(c_l2))
